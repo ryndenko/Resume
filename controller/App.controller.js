0b8a7ed1452695844ab8ee24cc6619af7aac21f1
Eldyby <98885431+Eldyby@users.noreply.github.com>
2023-06-06 18:09:57 +0300

    Update App.controller.js

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -3,7 +3,7 @@ sap.ui.define([
    "sap/ui/model/json/JSONModel"
 ], function (BaseController, JSONModel) {
    "use strict";
-   return BaseController.extend("pharelyshau.controller.App", {
+   return BaseController.extend("ryndenko.controller.App", {
 
       onInit: function () {
          // apply content density mode to root view
