0bd9148c98ff1031078548c68dbd1a7ebc0356b1
Vladimir Ryndenko <98885431+ryndenko@users.noreply.github.com>
2023-07-06 01:06:52 +0300

    Update formatter.js

--- model/formatter.js
+++ model/formatter.js
@@ -1,98 +1,153 @@
-sap.ui.define([
+sap.ui.define(
+	['sap/ui/core/format/DateFormat', 'sap/ui/core/Configuration'],
+	(DateFormat, Configuration) => {
+		'use strict';
 
-], function () {
+		return {
+			//////////////////////////////////
+			///////////// RESUME /////////////
+			//////////////////////////////////
 
-	"use strict";
+			stringDate(sDate) {
+				if (!sDate || sDate === 'Present') {
+					return this.i18n('lPresent');
+				}
+				return DateFormat.getDateInstance({ format: 'yMMM' }).format(new Date(sDate));
+			},
 
-	// private scope
-	const getPluralForm = function(nQuantity, sTextSingular, sTextPlural, sTextPlural2) {
-		if (!nQuantity || nQuantity <= 0) {
-			return "";
-		}
+			datesPeriod(sStartDate, sEndDate) {
+				if (!sStartDate || !sEndDate) {
+					return '';
+				}
 
-		if (nQuantity === 1) {
-			return nQuantity + "\u00A0" + sTextSingular;
-		}
+				const oStartDate = new Date(sStartDate);
+				const oEndDate = sEndDate === 'Present' ? new Date() : new Date(sEndDate);
+				// round up
+				oEndDate.setMonth(oEndDate.getMonth() + 1);
+				let nYears = oEndDate.getFullYear() - oStartDate.getFullYear();
+				let nMonths = oEndDate.getMonth() - oStartDate.getMonth();
 
-		// for Russian plural forms
-		if (localStorage.language === "ru") {
-			const sLastDigits = String(nQuantity).slice(-2);
-			const nLastDigits = sLastDigits > 20 ? +(sLastDigits.slice(-1)) : +sLastDigits; 
-			if (nLastDigits === 1) {
-				return nQuantity + "\u00A0" + sTextSingular;
+				if (nMonths < 0) {
+					nYears--;
+					nMonths += 12;
+				}
+				const aYearTexts = ['lYear', 'lYears', 'lYearPlural'].map((sKey) => this.i18n(sKey));
+				let sResult = this.formatter.getPluralForm(nYears, ...aYearTexts);
+				sResult += nMonths && nYears ? '\u00A0' : '';
+				const aMonthTexts = ['lMonth', 'lMonths', 'lMonthPlural'].map((sKey) => this.i18n(sKey));
+				sResult += this.formatter.getPluralForm(nMonths, ...aMonthTexts);
+				return sResult;
+			},
+
+			getPluralForm(nQuantity, sTextSingular, sTextPlural, sTextPlural2) {
+				if (!nQuantity || nQuantity <= 0) {
+					return '';
+				}
+
+				if (nQuantity === 1) {
+					return nQuantity + '\u00A0' + sTextSingular;
+				}
+
+				// for Russian plural forms
+				if (Configuration.getLanguage() === 'ru') {
+					const sLastDigits = String(nQuantity).slice(-2);
+					const nLastDigits = sLastDigits > 20 ? +sLastDigits.slice(-1) : +sLastDigits;
+					if (nLastDigits === 1) {
+						return nQuantity + '\u00A0' + sTextSingular;
+					}
+					if (nLastDigits > 4) {
+						return nQuantity + '\u00A0' + sTextPlural2;
+					}
+				}
+
+				return nQuantity + '\u00A0' + sTextPlural;
+			},
+
+			//////////////////////////////////
+			//////////// CALENDAR ////////////
+			//////////////////////////////////
+
+			// Input Validation
+
+			conferenceState(sConference) {
+				if (!sConference || /^(ftp|http|https):\/\/[^ "]+$/.test(sConference)) {
+					return 'None';
+				}
+				return 'Warning';
+			},
+
+			conferenceStateText(sConference) {
+				if (!sConference || /^(ftp|http|https):\/\/[^ "]+$/.test(sConference)) {
+					return '';
+				}
+				return 'The «Conference» field expects a URL';
+			},
+
+			// Apointment Formatting for JSON Model (calling from controller)
+
+			appointmentLocal(oAppointmentGC) {
+				const oAppointment = {
+					ID: oAppointmentGC.id,
+					Name: this.i18n('tBusy'),
+					Type: 'Type16',
+					Mode: 'view'
+				};
+				const aDates = [oAppointmentGC.start, oAppointmentGC.end];
+				this.formatter.setDatesToAppointmentLocal(oAppointment, ...aDates);
+				if (this.getAvailableAppointmentIDs().includes(oAppointment.ID)) {
+					this.formatter.setFieldsToAvailableAppointmentLocal(oAppointment, oAppointmentGC);
+				}
+
+				return oAppointment;
+			},
+
+			setDatesToAppointmentLocal(oAppointment, oStartDateGC, oEndDateGC) {
+				oAppointment.StartDate = new Date(oStartDateGC.dateTime ?? oStartDateGC.date + 'T00:00');
+				const oEndDate = new Date(oEndDateGC.dateTime ?? oEndDateGC.date + 'T00:00');
+				if (!oEndDateGC.dateTime) {
+					// set up all-day appointments for correct displaying
+					oEndDate.setDate(oEndDate.getDate() - 1);
+				}
+				oAppointment.EndDate = oEndDate;
+			},
+
+			setFieldsToAvailableAppointmentLocal(oAppointment, oAppointmentGC) {
+				const sEmail = oAppointmentGC.attendees ? oAppointmentGC.attendees[0].email : '';
+				oAppointment.Name = oAppointmentGC.summary;
+				oAppointment.Description = oAppointmentGC.description;
+				oAppointment.Email = sEmail;
+				oAppointment.Type = 'Type01';
+				oAppointment.GoogleMeet = oAppointmentGC.hangoutLink;
+				oAppointment.Conference = oAppointmentGC.location;
+			},
+
+			appointmentGC(oAppointment) {
+				const oAppointmentGC = {
+					summary: oAppointment.Name,
+					description: oAppointment.Description,
+					start: { dateTime: oAppointment.StartDate.toISOString() },
+					end: { dateTime: oAppointment.EndDate.toISOString() },
+					attendees: [{ email: oAppointment.Email }],
+					conferenceData: null,
+					location: null
+				};
+				if (oAppointment.GoogleMeet) {
+					this.setGoogleMeetToAppointmentGC(oAppointmentGC, oAppointment.ID);
+				} else {
+					oAppointmentGC.location = oAppointment.Conference;
+				}
+
+				return oAppointmentGC;
+			},
+
+			setGoogleMeetToAppointmentGC(oAppointmentGC, sAppointmentID) {
+				oAppointmentGC.conferenceData = {
+					createRequest: {
+						requestId: sAppointmentID,
+						conferenceSolutionKey: { type: 'hangoutsMeet' }
+					}
+				};
 			}
-			if (nLastDigits > 4) {
-				return nQuantity + "\u00A0" + sTextPlural2;
-			}
-		}
-
-		return nQuantity + "\u00A0" + sTextPlural;
+		};
 	}
-
-	return {
-
-		textList: function (aValues) {
-			if (!aValues) {
-				return "";
-			}
-			let sResult = "";
-			aValues.forEach(sValue => {
-				sResult += `• \u00A0${sValue}\n`;
-			});
-			return sResult;
-		},
-
-		stringDate: function (sDate) {
-			if (!sDate || sDate === "Present") {
-				return this.getResourceBundle().getText("lPresent");
-			}
-			return sap.ui.core.format.DateFormat.getDateInstance({
-				format: "yMMM"
-			}).format(new Date(sDate));
-		},
-
-		datesPeriod: function (sStartDate, sEndDate) {
-			if (!sStartDate) {
-				return "";
-			}
-
-			const oStartDate = new Date(sStartDate);
-			const oEndDate = !sEndDate || sEndDate == "Present" ? new Date() : new Date(sEndDate);
-			// round up
-			oEndDate.setMonth(oEndDate.getMonth() + 1);
-			let nYears = oEndDate.getFullYear() - oStartDate.getFullYear();
-			let nMonths = oEndDate.getMonth() - oStartDate.getMonth();
-
-			if (nMonths < 0) {
-				nYears--;
-				nMonths += 12;
-			}
-			let sResult = getPluralForm(nYears, this.getResourceBundle().getText("lYear"), this.getResourceBundle().getText("lYears"), this.getResourceBundle().getText("lYearPlural"));
-			sResult += nMonths && nYears ? "\u00A0" : "";
-			sResult += getPluralForm(nMonths, this.getResourceBundle().getText("lMonth"), this.getResourceBundle().getText("lMonths"), this.getResourceBundle().getText("lMonthPlural"));
-			return sResult;
-		},
-
-		textLink: function (sLink, sText) {
-			if (!sLink) {
-				return "";
-			}
-
-			if (!sText) {
-				return `<a href="${sLink}">${sLink}</a>`;
-			} 
-			
-			return `<a href="${sLink}">${sText}</a>`;
-		},
-
-		// UNUSED
-		numberUnit: function (sValue) {
-			if (!sValue) {
-				return "";
-			}
-			return parseFloat(sValue).toFixed(2);
-		}
-
-	};
-
-});
+);
